c8ffbac3fabfb9408a10abf2e95100e7a22f9c76
Daniel Wilson <wilsoda@umich.edu>
2014-10-16 15:31:47 -0400

    cleaned up Entry Controller

--- public/js/controllers/EntryCtrl.js
+++ public/js/controllers/EntryCtrl.js
@@ -1,60 +1,63 @@
 /**
  * Created by Daniel on 8/22/2014.
+ *  - Handles the entry page of DiscoCloud
  */
 angular.module('EntryCtrl', []).controller('EntryController', function($scope, $http, $location, User, socket) {
 
-    /// ===============================================
+    // ==== Entry Controller ==== //
+    $scope.query   = "";
+    $scope.results = {};
 
-    $scope.query = "";
-
-    $http.get("/api/all")
-        .success(function(data) {
-            console.log(data);
-            $scope.results = data;
-        })
-        .error(function(err) {
-            console.log(err)
-        });
-
-    $scope.enterRoom = function(name) {
-        $location.path("/" + name);
-    };
-
-    $scope.createRoom = function(rmNm) {
-        if(rmNm) {
-            $http.post("api/room/" + rmNm)
-                .success(function (data) {
-                    console.log(data);
-                    $scope.enterRoom(rmNm);
-                })
-                .error(function (err) {
-                    console.log(error);
-                })
-        }
-    };
-
-    socket.on('totalChange', function() {
+    // reload all room data
+    $scope.loadRooms = function() {
         $http.get("/api/all")
             .success(function(data) {
                 console.log(data);
                 $scope.results = data;
             })
             .error(function(err) {
-                console.log(err)
-            });
+                console.log(err);
+            })
+    }();
+
+    // enter an existing room
+    $scope.enterRoom = function(name) {
+        $location.path("/" + name);
+    };
+
+    // create a new room and enter
+    $scope.createRoom = function(rmNm) {
+        if (!rmNm) {
+            alert('Please Enter a Room Name');
+        } else {
+            $http.post("api/room/" + rmNm, {})
+                .success(function (data) {
+                    console.log(data);
+                    $scope.enterRoom(rmNm);
+                })
+                .error(function (err) {
+                    console.log(err);
+                })
+        }
+    };
+
+    // ==== Socket Events ==== //
+
+    // room data change event
+    socket.on('totalChange', function() {
+        $scope.loadRooms();
     });
 
+    // current users change
     socket.on('new', function(data) {
         $scope.userCount = data;
-    })
+    });
 
-    // ==== Utility ==== //
+    // ==== Filter Utility ==== //
 
+    // filters repeat by query text
     $scope.search = function (item){
-        if (item.name.toLowerCase().indexOf($scope.query.toLowerCase()) > -1) {
-            return true;
-        }
-        return false;
+        return item.name.toLowerCase().indexOf($scope.query.toLowerCase()) > -1;
     };
 
 });
